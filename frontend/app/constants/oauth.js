250603f0e855b765dcdd275a587f78dddca4aaef
Mustafa Taha Şahin <mustafatahasahin@gmail.com>
2021-12-21 21:26:28 +0300

    Set Oauth2 url

--- frontend/app/constants/oauth.js
+++ frontend/app/constants/oauth.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_BASE_URL = process.env.NEXT_PUBLIC_HOST_URL;
 
 export const OAUTH2_REDIRECT_URI =
   process.env.NEXT_PUBLIC_BASE_URL + '/oauth2/redirect';
